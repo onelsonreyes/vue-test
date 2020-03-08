039a0aa02dce9de27f6b77fa03cf1dd5e63713e4
onelson <onelson.reyes@gmailcom>
2020-03-08 14:03:06 -0600

    add cancel action

--- src/components/EmployeeTable.vue
+++ src/components/EmployeeTable.vue
@@ -13,10 +13,22 @@
       </thead>
       <tbody>
         <tr v-for="employee in employees" :key="employee.id">
-          <td>{{ employee.name }}</td>
-          <td>{{ employee.email }}</td>
-          <td>
-            <button >Edit</button>
+          <td v-if="editing === employee.id">
+            <input type="text" v-model="employee.name" />
+          </td>
+          <td v-else>{{ employee.name }}</td>
+
+          <td v-if="editing === employee.id">
+            <input type="text" v-model="employee.email" />
+          </td>
+          <td v-else>{{ employee.email }}</td>
+
+          <td v-if="editing === employee.id">
+            <button @click="editEmployee(employee)">Save</button>
+            <button class="muted-button" @click="cancelEdit(employee)">Cancel</button>
+          </td>
+          <td v-else>
+            <button @click="editMode(employee)">Edit</button>
             <button @click="$emit('delete:employee', employee.id)">Delete</button>
           </td>
         </tr>
@@ -28,9 +40,32 @@
 <script>
   export default {
     name: 'employee-table',
+    data() {
+      return {
+        editing: null,
+        cachedEmployee: null,
+      }
+    },
     props: {
       employees: Array,
-    }
+    },
+    methods: {
+      editMode(employee) {
+        this.cachedEmployee = Object.assign({}, employee);
+        this.editing = employee.id;
+      },
+
+      cancelEdit(employee) {
+        Object.assign(employee, this.cachedEmployee)
+        this.editing = null;
+      },
+
+      editEmployee(employee) {
+        if (employee.name === '' || employee.email === '') return
+        this.$emit('edit:employee', employee.id, employee)
+        this.editing = null
+      },
+    },
   }
 </script>
 
